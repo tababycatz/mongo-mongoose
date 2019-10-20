eb89f20b0a8017f0ff694275562ef3a9bad26d0f
Tabatha Poonpurmsiri <tabatha.pp@gmail.com>
2019-10-20 17:58:46 -0300

    updated folder structure and restarted server.js

--- server.js
+++ server.js
@@ -1,32 +1,61 @@
-//Dependencies//
+// Dependencies //
 var express = require("express");
+var logger = require("morgan");
 var mongoose = require("mongoose");
-var expressHandlebars = require("express-handlebars");
-var PORT = process.env.PORT || 3000;
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require("./models");
+var PORT = 3000;
+
 var app = express();
-var router = express.Router();
-    require("./config/routes")(router);
 
-app.use(express.static(__dirname + "/public"));
-app.use(router);
+// Use morgan logger for logging requests //
+app.use(logger("dev"));
+// Parse request body as JSON //
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder //
+app.use(express.static("public"));
 
-var db = process.eng.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
-mongoose.connect(db, function(error){
-    if(error) {
-        console.log(error);
-    } else {
-        console.log("mongoose is good to go")
-    }
+// Connect to Mongo DB //
+mongoose.connect("mongodb://localhost/mongoScrape", { useNewUrlParser: true });
+
+// Routes //
+
+app.get("/", function(req, res) {
+    res.send("Good to go!");
+  });
+
+app.get("/scrape", function(req, res){
+    axios.get("http://www.nytimes.com/").then(function(response){
+        var $ = cheerio.load(response.data);
+        $("article").each(function (i, element){
+            var result = {};
+            result.title = $(this).children("h2").text();
+            result.link = $(this).children("a").attr("href");
+
+            db.Article.create(result).then(function(dbArticle){
+                console.log(dbArticle);
+            })
+            .catch(function(err){
+                console.log(err);
+            });
+        });
+        res.send("Scrape done!");
+    });
 });
 
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.get("/articles", function(req, res){
+    db.Article.find({})
+    .then(function(dbArticle){
+        res.json(dbArticle);
+    })
+    .catch(function(err){
+        res.json(err);
+    });
 
-app.engine("handlebars", expressHandlebars({
-    defaultLayout: "main"
-}));
-app.set("view engine", "handlebars");
+});
 
 app.listen(PORT, function(){
     console.log("Listening on port:" + PORT);
